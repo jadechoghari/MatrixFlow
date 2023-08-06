9071f6e2414726a2abaa07e4b371bee32cd62b5d
Jade Choghari <chogharijade@icloud.com>
2023-08-06 23:59:48 +0300

    Initial Setup

---
+++ MatrixFlow/MatrixFlow.h
@@ -0,0 +1,18 @@
+//
+//  MatrixFlow.h
+//  MatrixFlow
+//
+//  Created by Jade Choghari on 06/08/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MatrixFlow.
+FOUNDATION_EXPORT double MatrixFlowVersionNumber;
+
+//! Project version string for MatrixFlow.
+FOUNDATION_EXPORT const unsigned char MatrixFlowVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MatrixFlow/PublicHeader.h>
+
+
---
+++ MatrixFlowExamples/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MatrixFlowExamples
+//
+//  Created by Jade Choghari on 06/08/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
